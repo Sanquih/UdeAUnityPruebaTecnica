542db9c4f636deb7aae070410d95da6cc7f5beec
Santiago Quintero Hincapié <53449686+Sanquih@users.noreply.github.com>
2025-03-06 09:04:36 -0500

    Adds SaveMager and ItemsManager

---
+++ UdeAUnityPruebaTecnica/Assets/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using BayatGames.SaveGameFree;
+using System.Collections.Generic;
+
+public class SaveManager : MonoBehaviour
+{
+    [SerializeField] private bool cleanData = false;
+
+    private string ITEMS_TAKEN_KEY = "ITEMSTAKEN";
+    public List<int> LoadItemsTaken()
+    {
+        return SaveGame.Exists(ITEMS_TAKEN_KEY)
+        ? SaveGame.Load<List<int>>(ITEMS_TAKEN_KEY)
+        : new List<int>();
+    }
+    public void SaveItemsTaken(List<int> value)
+    {
+        SaveGame.Save(ITEMS_TAKEN_KEY, value);
+    }
+
+    public static SaveManager Instance;
+    private void Awake()
+    {
+        if (cleanData) SaveGame.Clear();
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+}
