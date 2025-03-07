791f794752b8acc9df74845572c5c69e3b182ed9
Santiago Quintero Hincapié <53449686+Sanquih@users.noreply.github.com>
2025-03-07 16:48:33 -0500

    Adds Audio | Fix KeysSize and CameraFollow

--- UdeAUnityPruebaTecnica/Assets/Scripts/Controllers/ItemController.cs
+++ UdeAUnityPruebaTecnica/Assets/Scripts/Controllers/ItemController.cs
@@ -1,13 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
     [SerializeField] private int id;
+    [SerializeField] private AudioClip[] audios;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         if (ItemsManager.Instance.IsItemTaken(id)) Disappear(true);
-
+        else PlayRandomAudio();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,6 +19,18 @@ public class ItemController : MonoBehaviour
         {
             Disappear();
             ItemsManager.Instance.AddItemTaken(id);
+        }
+    }
+
+    private void PlayRandomAudio()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audios.Length > 0)
+        {
+            int randomIndex = Random.Range(0, audios.Length);
+            audioSource.clip = audios[randomIndex];
+            audioSource.Play();
         }
     }
 
@@ -35,5 +50,28 @@ public class ItemController : MonoBehaviour
         ParticleSystem ps = GetComponent<ParticleSystem>();
         var mainModule = ps.main;
         mainModule.loop = false;
+
+        FadeOutAudio(audioSource);
+    }
+
+    public void FadeOutAudio(AudioSource audioSource, float duration = 1f)
+    {
+        StartCoroutine(FadeOutCoroutine(audioSource, duration));
+    }
+
+    private IEnumerator FadeOutCoroutine(AudioSource audioSource, float duration)
+    {
+        float startVolume = audioSource.volume;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, 0f, elapsedTime / duration);
+            yield return null;
+        }
+
+        audioSource.volume = 0f;
+        audioSource.Stop();
     }
 }
--- UdeAUnityPruebaTecnica/Assets/Scripts/UI/UIHelpers.cs
+++ UdeAUnityPruebaTecnica/Assets/Scripts/UI/UIHelpers.cs
@@ -1,17 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.UIElements.Experimental;
 
 public class UIHelpers : MonoBehaviour
 {
     VisualElement root;
     UIDocument uiDocument;
-
-    float previousWidthFactor = -1f;
-    float previousHeightFactor = -1f;
 
     private bool isAnimating = true;
 
@@ -24,9 +19,7 @@ public class UIHelpers : MonoBehaviour
 
         ItemsManager.Instance.OnPokemonInfo += ShowName;
 
-        AdjustKeysSize();
         StartKeyAnimation();
-        RegisterResizeCallback();
     }
 
     private void OnDisable()
@@ -46,7 +39,7 @@ public class UIHelpers : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             keysPressed.Add("E");
-            Disappear("KeysInventory");
+            DisappearElement("KeysInventory");
             updated = true;
         }
 
@@ -57,7 +50,7 @@ public class UIHelpers : MonoBehaviour
     {
         bool hasAWD = keysPressed.Contains("A") && keysPressed.Contains("W") && keysPressed.Contains("D");
 
-        if (hasAWD) Disappear("KeysMuve");
+        if (hasAWD) DisappearElement("KeysMuve");
 
         if (hasAWD && keysPressed.Contains("E")) StopKeyAnimation();
     }
@@ -97,52 +90,6 @@ public class UIHelpers : MonoBehaviour
         }
 
         root.Remove(label);
-    }
-
-    private void AdjustKeysSize()
-    {
-        const float baseWidth = 1920f;
-        const float baseHeight = 1080f;
-        const float baseKeys = 150f;
-        const float baseKeysMuveWidth = 500f;
-        const float baseKeysMuveHeight = 450f;
-        const float baseKeysInventoryWidth = 500f;
-        const float baseKeysInventoryHeight = 300f;
-        const float baseBorderSolid = 11f;
-
-        float widthFactor = Mathf.Clamp(Screen.width / baseWidth, 0.5f, 2f);
-        if (Mathf.Approximately(widthFactor, previousWidthFactor)) return;
-        previousWidthFactor = widthFactor;
-
-        float heightFactor = Mathf.Clamp(Screen.height / baseHeight, 0.5f, 2f);
-        if (Mathf.Approximately(heightFactor, previousHeightFactor)) return;
-        previousHeightFactor = widthFactor;
-
-        var keys = root.Query<VisualElement>().Class("key").Build();
-        var keysMuve = root.Q<VisualElement>("KeysMuve");
-        var keysInventory = root.Q<VisualElement>("KeysInventory");
-
-        foreach (var key in keys)
-        {
-            key.style.width = baseKeys * widthFactor;
-            key.style.height = baseKeys * widthFactor;
-            key.style.borderTopWidth = baseBorderSolid * widthFactor;
-            key.style.borderBottomWidth = baseBorderSolid * widthFactor;
-            key.style.borderLeftWidth = baseBorderSolid * widthFactor;
-            key.style.borderRightWidth = baseBorderSolid * widthFactor;
-        }
-
-        keysMuve.style.width = baseKeysMuveWidth * widthFactor;
-        keysMuve.style.height = baseKeysMuveHeight * heightFactor;
-
-        keysInventory.style.width = baseKeysInventoryWidth * widthFactor;
-        keysInventory.style.height = baseKeysInventoryHeight * heightFactor;
-    }
-
-    private void RegisterResizeCallback()
-    {
-        var root = uiDocument.rootVisualElement;
-        root.RegisterCallback<GeometryChangedEvent>(evt => AdjustKeysSize());
     }
 
     private void StartKeyAnimation()
@@ -188,7 +135,7 @@ public class UIHelpers : MonoBehaviour
         isAnimating = false;
     }
 
-    private void Disappear(string id)
+    private void DisappearElement(string id)
     {
         var keysContainer = root.Q<VisualElement>(id);
         if (keysContainer != null)
